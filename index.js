24b39b116de70a08deb1bfdae044f7dd70ad5dbd
TonyMcKendry <TonyMcKendry@gmail.com>
2017-02-11 18:44:02 -0700

    loop

--- index.js
+++ index.js
@@ -11,24 +11,108 @@ var led9 = new Gpio(24, 'out')
 var led10 = new Gpio(25, 'out')
 var led11 = new Gpio(12, 'out')
 var led12 = new Gpio(16, 'out')
-	
- 
-setInterval(function(){
-	led1.writeSync(led1.readSync() === 0 ? 1 : 0)
-}, 100);
 
+let turnOn=()=>{
+  led1.writeSync(1)
+  setTimeout(()=>{
+    led2.writeSync(1)
+  }, 50)
+  setTimeout(()=>{
+    led3.writeSync(1)
+  }, 100)
+  setTimeout(()=>{
+    led4.writeSync(1)
+  }, 150)
+  setTimeout(()=>{
+    led5.writeSync(1)
+  }, 200)
+  setTimeout(()=>{
+    led6.writeSync(1)
+  }, 250)
+  setTimeout(()=>{
+    led7.writeSync(1)
+  }, 300)
+  setTimeout(()=>{
+    led8.writeSync(1)
+  }, 350)
+  setTimeout(()=>{
+    led9.writeSync(1)
+  }, 400)
+  setTimeout(()=>{
+    led10.writeSync(1)
+  }, 450)
+  setTimeout(()=>{
+    led11.writeSync(1)
+  }, 500)
+  setTimeout(()=>{
+    led12.writeSync(1)
+  }, 550)
+  setTimeout(()=>{
+    turnOff()
+  }, 600)
+}
 
-setTimeout(function(){
-setInterval(function(){
-        led2.writeSync(led1.readSync() === 0 ? 1 : 0)
-}, 100);
-}, 100)
+let turnOff=()=>{
+  led1.writeSync(0)
+  setTimeout(()=>{
+    led2.writeSync(0)
+  }, 50)
+  setTimeout(()=>{
+    led3.writeSync(0)
+  }, 100)
+  setTimeout(()=>{
+    led4.writeSync(0)
+  }, 150)
+  setTimeout(()=>{
+    led5.writeSync(0)
+  }, 200)
+  setTimeout(()=>{
+    led6.writeSync(0)
+  }, 250)
+  setTimeout(()=>{
+    led7.writeSync(0)
+  }, 300)
+  setTimeout(()=>{
+    led8.writeSync(0)
+  }, 350)
+  setTimeout(()=>{
+    led9.writeSync(0)
+  }, 400)
+  setTimeout(()=>{
+    led10.writeSync(0)
+  }, 450)
+  setTimeout(()=>{
+    led11.writeSync(0)
+  }, 500)
+  setTimeout(()=>{
+    led12.writeSync(1)
+  }, 550)
+}
 
-
- 
 // Stop blinking the LED and turn it off after 5 seconds.
 setTimeout(function() {
-    clearInterval(iv); // Stop blinking
     led1.writeSync(0);  // Turn LED off.
     led1.unexport();    // Unexport GPIO and free resources
+    led2.writeSync(0);  // Turn LED off.
+    led2.unexport();    // Unexport GPIO and free resources
+    led3.writeSync(0);  // Turn LED off.
+    led3.unexport();    // Unexport GPIO and free resources
+    led4.writeSync(0);  // Turn LED off.
+    led4.unexport();    // Unexport GPIO and free resources
+    led5.writeSync(0);  // Turn LED off.
+    led5.unexport();    // Unexport GPIO and free resources
+    led6.writeSync(0);  // Turn LED off.
+    led6.unexport();    // Unexport GPIO and free resources
+    led7.writeSync(0);  // Turn LED off.
+    led7.unexport();    // Unexport GPIO and free resources
+    led8.writeSync(0);  // Turn LED off.
+    led8.unexport();    // Unexport GPIO and free resources
+    led9.writeSync(0);  // Turn LED off.
+    led9.unexport();    // Unexport GPIO and free resources
+    led10.writeSync(0);  // Turn LED off.
+    led10.unexport();    // Unexport GPIO and free resources
+    led11.writeSync(0);  // Turn LED off.
+    led11.unexport();    // Unexport GPIO and free resources
+    led12.writeSync(0);  // Turn LED off.
+    led12.unexport();    // Unexport GPIO and free resources
 }, 12000)
