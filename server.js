4b8832cee303d9401d8f34862650e386b144c369
TonyMcKendry <TonyMcKendry@gmail.com>
2017-02-15 19:52:39 -0700

    settimeout

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ var server = http.createServer(function(request, response) {
 if (request.url === '/trigger' && request.method == 'GET') {
 // turn on the buzzer
 leds.forEach((l)=>{
-  console.log('on')
-  console.log(l)
   l.writeSync(1)
-})
-leds.forEach((l)=>{
-  console.log('off')
-  console.log(l)
-  l.writeSync(0)
 })
 
 // turn off the buzzer after 2 seconds
-// setTimeout(function() {
-// led.writeSync(0);
-// }, 2000);
+setTimeout(function() {
+  leds.forEach((l)=>{
+    l.writeSync(0)
+  })
+}, 2000);
 response.writeHeader(200, {
 "Content-Type": "application/json",
 "Access-Control-Allow-Origin": "*"
